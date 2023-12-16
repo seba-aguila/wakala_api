3ed92a8e891647dfc8cf794edb67df863b821e5b
seba-aguila <allarines@gmail.com>
2023-12-16 06:06:57 -0300

    Ajuste registro de usuarios

--- functions/src/controllers/userControllers.js
+++ functions/src/controllers/userControllers.js
@@ -10,9 +10,18 @@ const createUser = async (req, res) => {
     })
     return res.status(200).json("Usuario creado correctamente");
   } catch (error) {
-    console.log(error)
+    if (error.errorInfo.code == 'auth/uid-already-exists')
+      return res.status(500).json("Nombre de usuario ya registrado");
+    else if (error.errorInfo.code == 'auth/email-already-exists')
+      return res.status(500).json("Email ya registrado");
+
     return res.status(500).json("Error al crear usuario");
   }
 }
+
+/*
+auth/uid-already-exists
+auth/email-already-exists
+*/
 
 module.exports = { createUser }
